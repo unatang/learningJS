f0ce82ffc862815cbbc7c35b52bb436befe1f266
unatang <1589410802@qq.com>
2016-05-30 23:18:37 +0800

    练习添加事件

---
+++ JavaScriptDOM/11.js
@@ -0,0 +1,20 @@
+﻿window.onload = function()
+{
+    var pone = document.getElementsByClassName("pone");
+    for(i=0; i<pone.length; i++)
+    {
+        pone[i].onmouseover = function()
+        {
+            var source = this.getAttribute("title");
+            var placeholder = document.getElementById("placeholder");
+            placeholder.innerHTML = source;            
+        }
+        pone[i].onmouseout = function()
+        {
+           var placeholder = document.getElementById("placeholder");
+           placeholder.innerHTML = "查看全称";
+        }
+    }
+    
+   
+}
