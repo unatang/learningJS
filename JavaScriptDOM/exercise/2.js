91072b4b538a4d2d3c9be01d3fde4ef2ce24b479
unatang <1589410802@qq.com>
2016-06-04 15:57:15 +0800

    js画廊加深练习

---
+++ JavaScriptDOM/exercise/2.js
@@ -0,0 +1,23 @@
+﻿window.onload = function()
+{
+    var a = document.getElementsByTagName("a");
+    for(var i=0; i<a.length; i++)
+    {
+        a[i].onmouseover = function()
+        {
+            var href = this.getAttribute("href");
+            var title = this.getAttribute("title");
+            var description = document.getElementById("description");
+            var placeholder = document.getElementById("placeholder");
+            placeholder.setAttribute("src", href);
+            description.innerHTML = title;
+        }
+        
+        a[i].onmouseout = function()
+        {
+            var placeholder = document.getElementById("placeholder");
+            placeholder.setAttribute("src", "images/0.gif");
+            
+        }
+    }   
+}
---
+++ JavaScriptDOM/exercise/3.js
@@ -0,0 +1,21 @@
+﻿window.onload = function()
+{
+    var p = document.getElementById("para");
+    p.onclick = function()
+    {
+        var content = document.getElementById("content");
+        
+        for(var i=0; i<10; i++)
+        {
+            var p = document.createElement("p");
+            var txt = document.createTextNode("创建10个标签" + (i+1));
+            p.appendChild(txt);
+            content.appendChild(p);
+        }
+    }
+    
+        
+        
+    
+    
+}
