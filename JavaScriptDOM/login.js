1aefa6d15b37ac3bc1be682b6f7377a4916a05a7
unatang <1589410802@qq.com>
2016-06-15 07:15:30 +0800

    登录信息

---
+++ JavaScriptDOM/login.js
@@ -0,0 +1,14 @@
+﻿window.onload = function() {
+    var button = document.getElementById("button");
+    button.onclick = function() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        if (username == "" || password == ""){
+            alert("用户名密码不能为空");
+        } else if(username == "admin" && password == "admin"){
+            alert("登录成功");
+        } else {
+            alert("用户名或密码错误");
+        }
+    }
+}
